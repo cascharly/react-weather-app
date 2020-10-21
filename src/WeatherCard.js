29ae7f0c83a9a2aaca09150e356d9ab88370e520
cascharly <cascharly@gmail.com>
2020-10-21 17:44:15 -0300

    weather icons

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -3,9 +3,11 @@ import React from "react";
 function WeatherCard(props) {
   return (
     <div className="card">
-      <h1 className="city">Sydney</h1>
-      <h3 className="country">AU</h3>
-      <img className="icon" src="" alt="Weather Icon" />
+      <div className="location">
+        <h1 className="city">Sydney</h1>
+        <h3 className="country">AU</h3>
+      </div>
+      <img className="icon" src="./img/cloudy.png" alt="Weather Icon" />
       <h1 className="temp">20 °C</h1>
       <h3 className="condition">Clouds</h3>
     </div>
