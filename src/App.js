e543405aee4b1e92ffb5d4f3124d98e6dd722d0e
cascharly <cascharly@gmail.com>
2020-10-30 16:44:42 -0300

    08 Hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import WeatherCard from "./components/weatherCard/component";
 
 function App() {
-  const data = async () => {
+  const [query, setQuery] = useState("Melbourne, au");
+  const [weather, setWeather] = useState({
+    temp: null,
+    city: null,
+    condition: null,
+    country: null,
+  });
+
+  const data = async (q) => {
     const apiRes = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=fb1362db4fc9d77dbd51ebcca2fed300"
+      `https://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=fb1362db4fc9d77dbd51ebcca2fed300`
     );
     const resJSON = await apiRes.json();
     return resJSON;
   };
-  data().then(res=>{
-    console.log('The temp is : ' + res.main.temp)
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    data(query).then((res) => {
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.sys.country,
+      });
+    });
+  };
+
+  useEffect(()=>{
+    data(query).then((res) => {
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.sys.country,
+      });
+    });
   })
+
   return (
     <div className="App">
-      <WeatherCard temp={-15} condition="Clear" city="Sydney" country="AU" />
-      <WeatherCard temp={15} condition="Clouds" city="Melbourne" country="AU" />
-      <WeatherCard temp={30} condition="Fog" city="London" country="GB" />
+      <WeatherCard
+        temp={weather.temp}
+        condition={weather.condition}
+        city={weather.city}
+        country={weather.country}
+      />
+      <form>
+        <input value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button onClick={(e) => handleSearch(e)}>Search</button>
+      </form>
     </div>
   );
 }
